a317e99d7b90d23b646b3d29d21dcefa32a1a205
Orexx1 <49260274+Orexx1@users.noreply.github.com>
2019-04-03 20:15:50 -0700

    Update Celcius.java

--- Celcius.java
+++ Celcius.java
@@ -1,21 +1,21 @@
-
-
+/* This program is called by the Master program and calculates the 
+temperature into celcius and displays the result in the master */
 
 
 public class Celcius {
 	
-	
+	/* Base temp set for celcius */ 
 	double Temperature = 35; 
 		
 
 	
-		
+		/* Calculation performed to turn a user input into a temp in Celcius */
 		double getTemperature() {
 		return ((Temperature - 32)*5)/9;
 		
 		
 		
 
-}
+   }
 
 }
