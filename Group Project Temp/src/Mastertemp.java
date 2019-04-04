4044373d4c521495259641f5a52ffa98b139a52b
rmajano110 <49260386+rmajano110@users.noreply.github.com>
2019-04-03 21:22:44 -0600

    Update Mastertemp.java

--- Group Project Temp/src/Mastertemp.java
+++ Group Project Temp/src/Mastertemp.java
@@ -1,27 +1,32 @@
+/*
+* This program asks a user for a temperature value and converts it from Fahrenheit to Celcius
+* and Kelvin. It receives user's input and sends it to two different classes that do the 
+* convertion. It finally displays the converted temperatures back to the user.
+*/
 
-
+// Import Java Utilities
 import java.util.*;
 
 public class Mastertemp {
 
 	
 	public static void main(String[] args) {
-		// Create scanner
+	// Create scanner
         Scanner input = new Scanner(System.in);
         
-        //User inputs a temperature for C or F. 
+	// Ask user to enter a temperature in Fahrenheit. 
         
-        System.out.print("Enter a Temperature in F or C");
+        System.out.print("Enter a Temperature in Fahrenheit");
         double temperature1 = input.nextInt();
         double temperature2 = input.nextInt();
         
-        
-        
-        
+        // Create two objects           
         Celcius thermo1 = new Celcius();
-        Fahrenheit thermo2 = new Fahrenheit();
+        Kelvins thermo2 = new Kelvins();
         
+	// Displays output to user	
         System.out.println("Temperature in Celsius = " + temperature1.getTemperature());
+	System.out.println("Temperature in Kelvins = " + temperature1.getTemperature());
        
        
 	}
