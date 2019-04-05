0f49304fbf714258c168e8e9229e1752c71171da
rmajano110 <49260386+rmajano110@users.noreply.github.com>
2019-04-04 21:30:15 -0600

    Update MasterTemp with Kelvins.java

--- MasterTemp with Kelvins.java
+++ MasterTemp with Kelvins.java
@@ -1,67 +1,46 @@
-
 /*
 * This program asks a user for a temperature value and converts it from Fahrenheit to Celcius
-* and Kelvin. It receives user's input and sends it to two different classes that do the 
-* convertion. It finally displays the converted temperatures back to the user.
+* and Kelvins. It receives user's input and sends it to two different classes that do the 
+* conversion. It finally displays the converted temperatures back to the user.
 */
 
 // Import Java Utilities
 import java.util.*;
 
-//Master class for program
-public class Mastertemp {
+public class MasterTemp {
 
 	
 	public static void main(String[] args) {
-		// Create scanner
+		// Create scanner object
         Scanner input = new Scanner(System.in);
         
+        // Defining variables to be used by the program
+        boolean continueinput = true; 	// Boolean variable to determine if keeps asking for a right number
+        double temperature1 = 0;	// Variable for registering the user's input for temperature
+        
+        // Verify user's input
+        do {
+        	try {
+        		// Ask user to enter a temperature in Fahrenheit. 
+        		System.out.print("Enter a Temperature in Fahrenheit: ");
+        		temperature1 = input.nextDouble();
+        
+        		// Create two objects           
+        		Celcius thermo1 = new Celcius(temperature1);
+        		Kelvins thermo2 = new Kelvins(temperature1);
+        
+        		// Displays output to user	
+        		System.out.println("Temperature in Celsius = " + thermo1.conversion());
+        		System.out.println("Temperature in Kelvins = " + thermo2.conversion());
+        		continueinput = false;
+        	}
+        
+        	//check if input is wrong
+        	catch(InputMismatchException ex) {
+        		System.out.println("The input must be a Number. Please try again.");
+        		input.nextLine();
+        	}
+        }while (continueinput);
+	}
 
-        
-       
-        
-      
-		
-       
-	/* Code Block for reading/writing to a file
-	*/
-		  try {
-			  
-			  //User inputs a temperature for F. 
-        	 System.out.print("Enter a Temperature in F");
-            	 double temperature1 = input.nextDouble();
-			  
-			  //create new object called Celcius with temperature1
-            	 Celcius thermo1 = new Celcius(temperature1);
-			  
-			  //create new object called Kelvins with temperature1
-         	Kelvins temp = new Kelvins(temperature1);
-			  
-			  //Output the conversions
-         	 System.out.println("Temperature in Celsius = " + thermo1.Conversion());
-      		  System.out.println("Temperature in Kelvins = " + temp.Conversion());
-
-		}
-		//check if input is wrong
-		catch(InputMismatchException ex) {
-			System.out.println("Enter a Number");
-			input.nextLine();
-		}
-       
-        
-       		
-            
-            
-          
-		
-        
 }
-         
-       
-       
-}
-
-	
-
-
-
